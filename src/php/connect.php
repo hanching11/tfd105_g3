466ea59dc9a49038cc3581ec7d657673778b2df2
freedomban <xiangenius@gmail.com>
2022-04-22 19:13:35 +0800

    revise PDO connect sql database and php

--- src/php/connect.php
+++ src/php/connect.php
@@ -1,13 +1,13 @@
 <?php
 
     $db_host = "127.0.0.1";
-    $db_user = "root";
-    $db_pass = "password";
-    $db_name = "mydb";
+    $db_user = "tibamefe_since2021";
+    $db_pass = "vwRBSb.j&K#E";
+    $db_select = "tibamefe_tfd105g3";
 
     try{
         //建立資料庫連線物件
-        $dsn_link = new PDO("mysql:host={$db_host};dbname={$db_name};charset=utf8",$db_user, $db_pass);
+        $dsn_link = new PDO("mysql:host={$db_host};dbname={$db_select};charset=utf8",$db_user, $db_pass);
 
     }catch (PDOException $e) {
         print "資料庫連結失敗:{$e->getMessage()}";
